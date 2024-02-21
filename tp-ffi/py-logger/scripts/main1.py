d11b1c09e983e495cf8cadd4e1946a5d45e338d8
TODO <TODO>
2024-02-21 17:28:49 +0100

    binding

--- tp-ffi/py-logger/scripts/main1.py
+++ tp-ffi/py-logger/scripts/main1.py
@@ -1,8 +1,9 @@
-
+import Logger
 if __name__ == '__main__':
 
     print('TODO')
-
+    log = Logger.Logger()
+   # log.add_item("Info", "aa")
     # TODO create a logger and add some items
 
     # TODO report_by_added
