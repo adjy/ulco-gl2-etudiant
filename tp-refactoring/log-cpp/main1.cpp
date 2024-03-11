04c1c6712ad8e22d35849d0a109e7e5d4713970f
TODO <TODO>
2024-03-11 15:42:31 +0100

    c++

--- tp-refactoring/log-cpp/main1.cpp
+++ tp-refactoring/log-cpp/main1.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <ostream>
 #include <functional>
 #include <iostream>
 
@@ -10,6 +11,16 @@ int mul2(int n) {
     return n*2;
 }
 
+
+int mycomputeFile(int v0) {
+    std::ofstream ofs("log.txt");
+    ofs << "add3 " + std::to_string(v0) << std::endl;
+ 
+    const int v1 = add3(v0);
+    ofs << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    return v2;
+}
 int mycompute(int v0) {
     std::cout << "add3 " + std::to_string(v0) << std::endl;
     const int v1 = add3(v0);
@@ -23,6 +34,8 @@ int main() {
 
     const int res = mycompute(18);
     std::cout << res << std::endl;
+
+    std::cout<< mycomputeFile(18);
 
     return 0;
 }
--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -4,6 +4,7 @@
 
 using logFunc_t = std::function<void(const std::string &)>;
 
+
 int add3(int n) {
     return n+3;
 }
@@ -12,8 +13,30 @@ int mul2(int n) {
     return n*2;
 }
 
+int mycompute(logFunc_t logF, int v0) {
+    logF("add3 " + std::to_string(v0) + "\n");
+    const int v1 = add3(v0);
+    logF( "mul2 " + std::to_string(v1) + "\n");
+    const int v2 = add3(v1);
+    return v2;
+}
+
+
+
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
+
+    logFunc_t print = [](const std::string &contenu){
+        std::cout<<contenu;
+    };
+
+    std::ofstream ofs("log2.txt");
+    logFunc_t print2 = [&ofs](const std::string &contenu){
+        ofs <<contenu;
+    };
+    
+    std::cout<<mycompute(print, 2);
 
     return 0;
 }
